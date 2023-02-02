12a70d890d310ebbdc9804e8f16f20c86935b0f6
Callum Campbell <cc396@st-andrews.ac.uk>
2023-02-02 14:46:48 +0000

    Commit 8YearOld

---
+++ 8YearOld.sh
@@ -0,0 +1,21 @@
+Keyword: hello
+    Decompostion Rule: *
+    Reassembly Rule: Hiya
+
+Keyword: how
+    Decompostion Rule: How are you *
+    Reassembly Rule: Im doing amazing, how are you?
+    Decompostion Rule:
+
+Keyword: older
+    Decompostion Rule: What do you want to be when your older *
+    Reassembly Rule: I want to be a astrounaut
+    Reassembly Rule: I want to be a zoo keeper
+    Reassembly Rule: I want to be a docter
+    Decompostion Rule: I want to be * when im older
+    Reassembly Rule: That sounds cool, what is a *
+    Reassembly Rule: A * that sounds awesome
+
+Keyword: goodbye
+    Decompostion Rule: *
+    Reassembly Rule: Bye Bye!!
