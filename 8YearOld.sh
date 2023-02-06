c2a88af8aee683b45239bbe973cea7a4c58de834
Callum Campbell <cc396@st-andrews.ac.uk>
2023-02-06 11:44:52 +0000

    Commit changes

--- 8YearOld.sh
+++ 8YearOld.sh
@@ -10,12 +10,29 @@ Key: how
 
 Key: older
     Decomp: What do you want to be when your older*
-        Reassem: I want to be a astrounaut
-        Reassem: I want to be a zoo keeper
+        Reassem: I want to be a astrounaut, space is soooo cool
+        Reassem: I want to be a zoo keeper because I love animals!
         Reassem: I want to be a docter
     Decomp: I want to be * when im older
-        Reassem: That sounds cool, what is a*
-        Reassem: A *that sounds awesome
+        Reassem: That sounds cool, what is a *
+        Reassem: Cool, why do you want to be a *
+        Reassem: A * that sounds awesome
+
+Key: school
+    Decomp: Do you like school
+        Reassem: School is super boring
+        Reassem: School is really fun, what do you think?
+    Decomp: I think school is good
+        Reassem: Same what do you like to do at school?
+    Decomp: I think school is *
+        Reassem: I think school is * too
+        Reassem: Why do think school *
+
+Key: animal
+    Decomp: What is your favourite animal?
+        Reassem: I really like giraffes, they are so tall!
+        Reassem: Elephants are awesome there trunks go bahruuuuuuuuuuhaaaaa
+        Reassem: Penguins are my favourite
 
 Key: happy
     Decomp: * makes me happy
@@ -25,6 +42,12 @@ Key: happy
         Reassem: Why are you happy?
         Reassem: What has made you happy?
         Reassem: Im happy too
+
+Key: like
+    Decomp: I like*
+        Reassem: I like * too
+        Reassem: What about * do you like
+        Reassem: Huh, what is *
 
 Key: sad
     Decomp: *Im sad*
@@ -38,3 +61,8 @@ Key: sad
 Key: goodbye
     Decomp: *
         Reassem: Bye Bye!!
+
+Key: *
+    Decomp: Huh?
+    Decomp: Im dont know what that is
+    Decomp: What do you mean?
