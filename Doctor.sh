ca5c41ee11ae934f9d4d4b2f0def48286583b95c
Ayush <as645@st-andrews.ac.uk>
2023-02-07 02:59:32 +0000

    Start of Doctor Script

---
+++ Doctor.sh
@@ -0,0 +1,69 @@
+
+
+Key: [hH]app[yi]\\*
+    Decomp: [nN]ot [hH]appy
+        Reassem: Why are you unhappy?
+        Reassem: Are you always  unhappy?
+        Reassem: When did you first start to feel unhappy?
+        Reaseem:
+    Decomp: [uU]nhappy
+        Reassem: Why are you unhappy?
+        Reassem: Are you always  unhappy?
+        Reassem: When did you first start to feel unhappy?
+    Decomp: [nN]ever happy
+        Reassem: Do you want to be happy?
+        Reassem: Are you unhappy?
+        Reassem: Do you think you will ever be happy?
+    Decomp: [hH]appiness
+        Reassem: HappyLol
+    Decomp: \\*
+        Reassem: Are you always happy?
+        Reassem: When do you feel happy?
+        Reassem: Do you want to be happy? 
+
+
+Key: [sS]uicid[ea]\\*
+    Decomp: [sS]uicide
+        Reassem: Do you think about suicide often?
+        Reassem: Have you tried to commit suicide?
+        Reassem: Suicide is not the answer.
+    Decomp: [sS]uicidal
+        Reassem: How long have you felt suicidial?
+        Reaseem: Is suicide reasonable to you?
+        Reassem: Why do you feel suicidal 
+Key: I
+    Decomp: 
+        Reassem:        
+Key: 
+    Decomp: 
+        Reassem:                
+Key: 
+    Decomp: 
+        Reassem:        
+Key: 
+    Decomp: 
+        Reassem:
+Key: [hH]ello
+    Decomp: [hH]ello \\* [mM]y name is \\*
+        Reassem: Hello, What brings you here? I'm Eliza.
+        Reassem: I'm Eliza, Hello!
+    Decomp: \\*
+        Reassem: Hello.
+        Reassem: Hey.
+        Reassem: Hi.
+Key: [hH]i
+    Decomp: [hH]i \\* name \\*
+        Reassem: Hello, What brings you here? I'm Eliza.
+        Reassem: I'm Eliza, Hello!
+    Decomp: \\*
+        Reassem: Hello.
+        Reassem: Hey.
+        Reassem: Hi.
+Key: [hH]ey
+    Decomp: [hH]ey \\* [mM]y name is \\*
+        Reassem: Hello, What brings you here? I'm Eliza.
+        Reassem: I'm Eliza, Hello!
+    Decomp: \\*
+        Reassem: Hello.
+        Reassem: Hey.
+        Reassem: Hi.
