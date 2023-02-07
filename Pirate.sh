8f83aa48adcc2b2df289328d18b83e6ace1d38e0
Callum Campbell <cc396@st-andrews.ac.uk>
2023-02-07 19:53:15 +0000

    Add pirate script

---
+++ Pirate.sh
@@ -0,0 +1,20 @@
+Key: [Hh]ello
+    Decomp: [Hh]ello
+        Reassem: Ahoy matey!
+
+Key: [Hh]ow
+    Decomp: [Hh]ow are you
+        Reassem: Im good lad, how bout yourself?
+        Reassem: Been feelina bit scurvy! What about you?
+
+Key: 
+
+Key: [Gg]oodbye
+    Decomp: [Gg]oodbye
+        Reassem: Goodbye
+
+Key: D34F4L7
+    Decomp: \\*
+        Reassem: Huh?
+        Reassem: Im not sure what you mean matey
+        Reassem: What do you mean, lad?
