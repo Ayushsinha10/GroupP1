9271027aaaa66616c11bfb8011ee5facbe55db1d
Callum Campbell <cc396@st-andrews.ac.uk>
2023-02-08 15:57:02 +0000

    Pirate script changes

--- Pirate.sh
+++ Pirate.sh
@@ -5,9 +5,20 @@ Key: [Hh]ello
 Key: [Hh]ow
     Decomp: [Hh]ow are you
         Reassem: Im good lad, how bout yourself?
-        Reassem: Been feelina bit scurvy! What about you?
+        Reassem: Been feelin a bit scurvy! What about you?
 
-Key: 
+Key: [Gg]ood
+    Decomp: [Gg]ood
+        Reassem: Im glad let us set sail!
+        Reassem: Perfect, ready to plunder and pilage the seven seas
+
+Key: [Hh]appy
+    Decomp: [Hh]appy
+        Reassem: Good to here me heartie
+        Reassem: And why might that be laddie
+    decomp: [Uu]nhappy
+        Reassem: Well thats nea good
+        Reassem: Cheer up laddie, canne have you bringing down the crew
 
 Key: [Gg]oodbye
     Decomp: [Gg]oodbye
